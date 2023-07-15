ef33ab7ec4b076e98d26f06961dacb826cc9b422
abu <abu@example.com>
2023-07-15 20:33:17 +0400

    WIP: replication

---
+++ internal/log/replicator.go
@@ -0,0 +1,54 @@
+package log
+
+import (
+	"sync"
+
+	api "github.com/kentliuqiao/proglog/api/v1"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+type Replicator struct {
+	DialOptions []grpc.DialOption
+	LocalServer api.LogClient
+
+	logger *zap.Logger
+
+	mu      sync.Mutex
+	servers map[string]chan struct{}
+	closed  bool
+	close   chan struct{}
+}
+
+func (r *Replicator) Join(name, addr string) error {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.init()
+
+	if r.closed {
+		return nil
+	}
+
+	if _, ok := r.servers[name]; ok {
+		return nil
+	}
+	r.servers[name] = make(chan struct{})
+
+	go r.replicate(addr, r.servers[name])
+
+	return nil
+}
+
+func (r *Replicator) replicate(addr string, leave chan struct{}) {}
+
+func (r *Replicator) init() {
+	if r.logger == nil {
+		r.logger = zap.L().Named("replicator")
+	}
+	if r.servers == nil {
+		r.servers = make(map[string]chan struct{})
+	}
+	if r.close == nil {
+		r.close = make(chan struct{})
+	}
+}
